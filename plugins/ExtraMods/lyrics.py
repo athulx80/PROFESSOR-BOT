39b8afc4ddce711c94132150fc81a7d64eebb7d9
Athul <103881131+athulx80@users.noreply.github.com>
2024-04-02 12:13:09 +0530

    Update lyrics.py

--- plugins/ExtraMods/lyrics.py
+++ plugins/ExtraMods/lyrics.py
@@ -17,9 +17,9 @@ async def sng(bot, message):
           await mee.delete()
           try:
             await mee.delete()
-            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/mkn_bots_updates")]]))
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🚦 CHANNEL", url = f"https://t.me/+L8SWfrF_7m04ODZl")]]))
           except Exception as e:                            
-             await message.reply_text(f"I Cᴀɴ'ᴛ Fɪɴᴅ A Sᴏɴɢ Wɪᴛʜ `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/mkn_bots_updates")]]))
+             await message.reply_text(f"I Cᴀɴ'ᴛ Fɪɴᴅ A Sᴏɴɢ Wɪᴛʜ `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("📺 MOVIES", url = f"https://t.me/+hC5tRAvQHHplMWQ1")]]))
 
 
 def search(song):
